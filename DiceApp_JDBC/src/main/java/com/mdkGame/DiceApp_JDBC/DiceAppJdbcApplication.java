711d84726d43a295d4d3e2574f00fdf4a3ce3288
yomandrake <yomandrake@gmail.com>
2020-05-05 19:26:41 +0200

    Project restart of Dice Game with Security.From Initializer and running with H2 Database and first hardcoding user and admin user to test project

---
+++ DiceApp_JDBC/src/main/java/com/mdkGame/DiceApp_JDBC/DiceAppJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.mdkGame.DiceApp_JDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiceAppJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiceAppJdbcApplication.class, args);
+	}
+
+}
---
+++ DiceApp_JDBC/src/test/java/com/mdkGame/DiceApp_JDBC/DiceAppJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdkGame.DiceApp_JDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiceAppJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
